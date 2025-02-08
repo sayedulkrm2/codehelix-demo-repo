4875473b4389ce573ca7f318e5b1cd0da39d42ca
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2025-02-09 02:11:13 +0600

    Fix vulnerability in lfi4.php

--- File Inclusion/lfi4.php
+++ File Inclusion/lfi4.php
@@ -1,4 +1,4 @@
-<?php     include("../common/header.php");   ?>
+<?php include_once('includes/class_'.basename($_GET['class']).'.php'); ?>
 
 <!-- from http://www.ush.it/2009/02/08/php-filesystem-attack-vectors/ -->
 <?php hint("will include the arg specified in the GET parameter \"class\", appends .php to end, defeat with NULL byte %00"); ?>
